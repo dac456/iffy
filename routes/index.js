e1e51491c4ee1a4552ca734e3afca2f9eb8170c7
Daniel Cook <dac456@mun.ca>
2014-10-30 19:47:29 -0230

    swapped TPB test for YIFY

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,39 @@ var https = require('https');
 var db = require('../db');
 
 exports.index = function(req, res){
-    https.get("https://yts.re/api/list.json", function(res) {
-        res.on('data', function(data) {
-            var obj = JSON.parse(data);
-            console.log(obj);
+    var entries = [];
+    https.get("https://yts.re/api/list.json?limit=50&sort=alphabet&order=asc", function(yts_res) {
+        var data = '';
+
+        yts_res.on('data', function (chunk){
+            data += chunk;
         });
+
+        yts_res.on('end',function(){
+            var obj = JSON.parse(data);
+            for(var i = 0; i < 50; i++) {
+                if(obj['MovieList'][i]['Quality'] === '1080p') {
+                    entries.push({
+                        title: obj['MovieList'][i]['MovieTitle'],
+                        maglink: obj['MovieList'][i]['TorrentMagnetUrl'],
+                        image: obj['MovieList'][i]['CoverImage']
+                    });
+                }
+            }
+
+            res.render('layouts/default', { 
+                title: 'Iffy',
+                entries: entries,
+                partials: {
+                    content: 'index'
+                } 
+            });
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
     });
     //TODO: this will eventually not be the index page
-    var entries = [];
+    /*var entries = [];
     db.getTpb(function(rows) {
         for(var i = 0; i < rows.length; i++) {
             entries.push({
@@ -26,7 +51,7 @@ exports.index = function(req, res){
                 content: 'index'
             } 
         });
-    });
+    });*/
 };
 
 exports.bt = function(req, res){
